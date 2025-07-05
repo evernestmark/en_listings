401650310a29bef7afd3b52cf864211f767b561c
Mark Lawrence Laroza Mangawang <mmangawang@evernest.co>
2025-07-05 13:00:47 +0800

    Removed redirect.js

--- redirect.js
+++
@@ -1,7 +0,0 @@
-  const currentPath = window.location.pathname;
-  const currentQuery = window.location.search;
-
-  if (currentPath === '/location-homes-for-rent/listings' && 
-      !currentQuery.includes('market=location')) {
-    window.location.href = '/location-homes-for-rent/listings?market=location';
-  }
